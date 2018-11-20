823b6ecb1c1cfa6993a701ade6645f18cb04f26a
hwang <hwang@hwang-T5500>
2018-11-20 10:46:25 +0900

    updated

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
-echo 'Installing TICK Stack on Raspberry Pi'
+echo "Installing TICK Stack on Raspberry Pi 3 Stretch..."
 
 sudo apt-get install curl
-sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add –
+sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 sudo apt-get update
 sudo apt-get install influxdb chronograf kapacitor telegraf
+echo "Now Rebooting.."
 sudo reboot
